f6cf1834153dc10c759b551893c4c4abd50bce86
baochuquan <baochuquan@163.com>
2016-03-14 16:00:41 +0800

    instructionRead(), memoryRead()测试完毕

--- Memory.cpp
+++ Memory.cpp
@@ -3,13 +3,25 @@
 void Memory::memoryClear(){
 	memory.clear();
 };
-	
+
+/**
+ * Write bytes to memory, default 1 bytes. Do: [addr] = source[0], [addr+1] = source[1]...
+ * @param addr   : The addr in memory begin to write.
+ * @param source : The data to be written into memory.
+ * @param bytes  : How many bytes will be written.
+ */
 void Memory::memoryWrite(unsigned int addr, unsigned char* source, unsigned int bytes){
 	for (unsigned int i = 0; i < bytes; i++){
 		memory[addr+i] = source[i];
 	}
 };
 
+/**
+ * Read bytes from memory, default 1 bytes. D0: [target] = [addr], [target+1] = [addr+1]...
+ * @param addr   : The addr in memory begin to read.
+ * @param target : The recevier buf to gain data.
+ * @param bytes  : How many bytes will be read.
+ */
 void Memory::memoryRead( unsigned int addr, unsigned char* target, unsigned int bytes){
 	for (unsigned int i = 0; i < bytes; i++){
 		if(memory.find(addr+i) != memory.end()){
@@ -21,6 +33,12 @@ void Memory::memoryRead( unsigned int addr, unsigned char* target, unsigned int 
 	}
 };
 
+/**
+ * Read one instruction frommemory.
+ * @param addr   : The instruction addr in memory.
+ * @param target : The receiver buf to gain the instruction.
+ * @param num    : How many instruction will be read.
+ */
 void Memory::instructionRead( unsigned int addr, unsigned int* target, unsigned int num){
 	unsigned int instruction = 0;
 	unsigned int onebyte;
@@ -32,9 +50,9 @@ void Memory::instructionRead( unsigned int addr, unsigned int* target, unsigned 
 			break;
 		}
 
-		if(i % 4 != 3){
-			instruction = instruction | (onebyte << (i%4 * 8));
-		} else {
+		instruction = instruction | (onebyte << (i%4 * 8));
+
+		if(i % 4 == 3){
 			target[i/4] = instruction;
 			instruction = 0;
 			onebyte = 0;
--- Simulator.cpp
+++ Simulator.cpp
@@ -17,11 +17,34 @@ int main(){
 	Memory memory;
 	unsigned int entryPoint; 					// the entry point of program 
 	entryPoint = loadELF(filename, memory);
-///*
-	cout << "entryPoint: 0x"   << hex << entryPoint << endl;
+
+///* Print entry Point, .bssBegin, .bssEnd
+	cout << "entryPoint: 0x" << hex << entryPoint << endl;
 	cout << ".bssBegin:  0x" << hex << bssBegin << endl;
 	cout << ".bssSize:   "   << dec << bssSize << " bytes" << endl;
 //*/
+
+/* Test instructionRead()
+	unsigned int instruction[20];
+	unsigned addr = 0x00010000;
+	memory.instructionRead(addr, instruction, 20);
+	for(int i = 0; i < 20; i++){
+		cout << "Addr: "   << hex << setw(8) << setfill('0') << addr+i << "    ";
+		cout << "Inst: "   << hex << setw(8) << setfill('0') << instruction[i] << endl;
+	}
+*/
+
+/* Test memoryRead()
+	unsigned char bytes[20];
+	unsigned addr = 0x00010000;
+	memory.memoryRead(addr, bytes, 20);
+	for(int i = 0; i < 20; i++){
+		cout << "Addr: "   << hex << setw(8) << setfill('0') << addr+i << "    ";
+		cout << "Byte: "   << hex << setw(2) << setfill('0') << (unsigned int)bytes[i] << endl;
+	}
+*/
+
+	
 
  	return 0;
 }
