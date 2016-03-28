db0257d4dbe2e05bddf9c6e0eca3b85fb812f44d
bale-john <1401214269@pku.edu.cn>
2016-03-28 14:29:12 +0800

    try dhrystone

--- doSyscall.cpp
+++ doSyscall.cpp
@@ -2,12 +2,24 @@
 #include <iostream>
 #include <cstdio>
 #include "Memory.h"
+#include "syscall.h"
 using namespace std;
 
 void doSyscall(vector<unsigned int>& reg, Memory& memory){
 	switch (reg[17]){
-		case 64:
+		case SYS_write:
+			cout << reg[12] << endl;
 			printf("%c", memory.memory[reg[11]]);
+			cout << "##########" << endl;
 			break;
+		/*
+		case SYS_fstat:
+
+		case SYS_brk:
+
+		case SYS_close:
+
+		case 
+		*/
 	}
 }
