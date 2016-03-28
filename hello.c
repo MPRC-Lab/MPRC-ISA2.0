14f0ddd02dfe3db33e2886f22fe94663a2d9db45
bale-john <1401214269@pku.edu.cn>
2016-03-28 12:38:29 +0800

    debug dead loop

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main(){
+	char s='i';
+	char b='d';
+	int d=3;
+	int k=19;
+	int o=19;
+	printf("no enter");
+	printf("ID is %c%c\n",s,b);
+	printf("cccccccc\n");
+	printf("d is %d\n",d);
+	printf("k is 0x%x\n",k);
+	printf("o is O%o\n",o);
+	printf("come back again\n");
+}
