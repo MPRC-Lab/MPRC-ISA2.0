c025fc9b318785d38d0c4afc2a41beaeee261db8
baochuquan <baochuquan@163.com>
2016-03-28 10:19:34 +0800

    compile

--- test9.c
+++ test9.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 
 int main(){
-	char s[] = "Hello";
-	printf("abc:%s", s);
+	char s[] = "Hello World!";
+//	int num = 666;
+//	printf("abc:%s %d", s,num);
+	printf("Greet:%s", s);
 	return 0;
 }
